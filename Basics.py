74c57cdcc88d788b395139a043321bfed86f5c5a
Elena Lizanets <60507262+stenazil@users.noreply.github.com>
2020-04-06 01:24:23 +0600

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## Hacker News project
-# 
-# *Hacker News* is a site started by the startup incubator Y Combinator, where user-submitted stories (known as "posts") are voted and commented upon, similar to reddit. Hacker News is extremely popular in technology and startup circles, and posts that make it to the top of Hacker News' listings can get hundreds of thousands of visitors as a result.
-# 
-# We're specifically interested in posts whose titles begin with either **Ask HN** or **Show HN**.
-# 
-# Users submit **Ask HN** posts to ask the Hacker News community a specific question.
-# Likewise, users submit **Show HN** posts to show the Hacker News community a project, product, or just generally something interesting.
-# 
-# We'll compare these two types of posts to determine the following:
-# 
-#  - Do **Ask HN** or **Show HN** receive more comments on average?
-#  - Do posts created at a certain time receive more comments on average?
+#Hacker News project
+
+#Hacker News is a site started by the startup incubator Y Combinator, where user-submitted stories (known as "posts") are voted and commented upon, similar to reddit. Hacker News is extremely popular in technology and startup circles, and posts that make it to the top of Hacker News' listings can get hundreds of thousands of visitors as a result.
+#We're specifically interested in posts whose titles begin with either Ask HN or Show HN.
+ 
+#Users submit Ask HN posts to ask the Hacker News community a specific question.
+#Likewise, users submit Show HN posts to show the Hacker News community a project, product, or just generally something interesting.
+ 
+#We'll compare these two types of posts to determine the following:
+ 
+ # - Do **Ask HN** or **Show HN** receive more comments on average?
+ #- Do posts created at a certain time receive more comments on average?
 
 # In[1]:
 
@@ -163,7 +162,6 @@ for avg, hr in sorted_swap[:5]:
 # 
 # However, it should be noted that the data set we analyzed excluded posts without any comments. Given that, it's more accurate to say that of the posts that received comments, ask posts received more comments on average and ask posts created between 15:00 and 16:00 (3:00 pm est - 4:00 pm est) received the most comments on average.
 
-# In[ ]:
 
 
 
