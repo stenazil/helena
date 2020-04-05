feaffdcbee046f86781297ef62238b666d057fd1
Elena Lizanets <60507262+stenazil@users.noreply.github.com>
2020-04-06 01:19:28 +0600

    Add files via upload
    Hacker News is a site started by the startup incubator Y Combinator, where user-submitted stories (known as "posts") are voted and commented upon, similar to reddit. Hacker News is extremely popular in technology and startup circles, and posts that make it to the top of Hacker News' listings can get hundreds of thousands of visitors as a result.
    
    We're specifically interested in posts whose titles begin with either Ask HN or Show HN.

---
+++ Basics.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Hacker News project
+# 
+# *Hacker News* is a site started by the startup incubator Y Combinator, where user-submitted stories (known as "posts") are voted and commented upon, similar to reddit. Hacker News is extremely popular in technology and startup circles, and posts that make it to the top of Hacker News' listings can get hundreds of thousands of visitors as a result.
+# 
+# We're specifically interested in posts whose titles begin with either **Ask HN** or **Show HN**.
+# 
+# Users submit **Ask HN** posts to ask the Hacker News community a specific question.
+# Likewise, users submit **Show HN** posts to show the Hacker News community a project, product, or just generally something interesting.
+# 
+# We'll compare these two types of posts to determine the following:
+# 
+#  - Do **Ask HN** or **Show HN** receive more comments on average?
+#  - Do posts created at a certain time receive more comments on average?
+
+# In[1]:
+
+
+#read and open file
+
+from csv import reader
+opened_file = open('hacker_news.csv')
+read_file = reader(opened_file)
+hn = list(read_file)
+print(hn[:5])
+
+
+# In[2]:
+
+
+headers = hn[0]
+hn = hn[1:]
+print(headers)
+print(hn[:5])
+
+
+# In[3]:
+
+
+ask_posts = []
+show_posts = []
+other_posts = []
+
+for post in hn:
+    title = post[1]
+    if title.lower().startswith('ask hn'):
+        ask_posts.append(post)
+    elif title.lower().startswith('show hn'):
+        show_posts.append(post)
+    else:
+        other_posts.append(post)
+        
+print(len(ask_posts))
+print(len(show_posts))
+print(len(other_posts))
+        
+    
+
+
+# In[4]:
+
+
+total_ask_comments = 0
+
+for post in ask_posts:
+    total_ask_comments += int(post[4])
+    
+avg_ask_comments = total_ask_comments / len(ask_posts)
+print(avg_ask_comments)
+
+
+# In[5]:
+
+
+total_show_comments = 0
+
+for post in show_posts:
+    total_show_comments += int(post[4])
+    
+avg_show_comments = total_show_comments / len(show_posts)
+print(avg_show_comments)
+
+
+# On average, ask posts in our sample receive approximately 14 comments, whereas show posts receive approximately 10. Since ask posts are more likely to receive comments, we'll focus our remaining analysis just on these posts.
+
+# In[6]:
+
+
+# Calculate the amount of ask posts created during each hour of day and the number of comments received.
+import datetime as dt
+
+result_list = []
+
+for post in ask_posts:
+    result_list.append([post[6],
+                        int(post[4])]
+                      )
+
+comments_by_hour = {}
+counts_by_hour = {}
+date_format = "%m/%d/%Y %H:%M"
+
+for each_row in result_list:
+    date = each_row[0]
+    comment = each_row[1]
+    time = dt.datetime.strptime(date, date_format).strftime("%H")
+    if time in counts_by_hour:
+        comments_by_hour[time] += comment
+        counts_by_hour[time] += 1
+    else:
+        comments_by_hour[time] = comment
+        counts_by_hour[time] = 1
+
+print(comments_by_hour)
+    
+
+
+# In[7]:
+
+
+# Calculate the average amount of comments `Ask HN` posts created at each hour of the day receive.
+avg_by_hour = []
+
+for hr in comments_by_hour:
+    avg_by_hour.append([hr, comments_by_hour[hr] / counts_by_hour[hr]])
+
+avg_by_hour
+
+
+# In[8]:
+
+
+swap_avg_by_hour = []
+
+for row in avg_by_hour:
+    swap_avg_by_hour.append([row[1], row[0]])
+    
+print(swap_avg_by_hour)
+
+sorted_swap = sorted(swap_avg_by_hour, reverse=True)
+
+sorted_swap
+
+
+# In[9]:
+
+
+# Sort the values and print the the 5 hours with the highest average comments.
+
+print("Top 5 Hours for 'Ask HN' Comments")
+for avg, hr in sorted_swap[:5]:
+    print(
+        "{}: {:.2f} average comments per post".format(
+            dt.datetime.strptime(hr, "%H").strftime("%H:%M"),avg
+        )
+    )
+
+
+# ## Conclusion
+# In this project, we analyzed ask posts and show posts to determine which type of post and time receive the most comments on average. Based on our analysis, to maximize the amount of comments a post receives, we'd recommend the post be categorized as ask post and created between 15:00 and 16:00 (3:00 pm est - 4:00 pm est).
+# 
+# However, it should be noted that the data set we analyzed excluded posts without any comments. Given that, it's more accurate to say that of the posts that received comments, ask posts received more comments on average and ask posts created between 15:00 and 16:00 (3:00 pm est - 4:00 pm est) received the most comments on average.
+
+# In[ ]:
+
+
+
+
